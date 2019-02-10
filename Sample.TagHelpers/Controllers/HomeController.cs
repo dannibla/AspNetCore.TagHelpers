2e64bc3dbb177dee939d7596fc916066f8dd9df3
Daniel J Abraham <dannibla@outlook.com>
2019-02-10 22:19:28 +0530

    removed cookie partial

--- Sample.TagHelpers/Controllers/HomeController.cs
+++ Sample.TagHelpers/Controllers/HomeController.cs
@@ -15,10 +15,5 @@ namespace Sample.TagHelpers.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
